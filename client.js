71a68a60374190bddd2195006430301e9af0a934
Thomas Johander <lopan@Thomas-Johanders-MacBook-Pro.local>
2017-02-14 23:39:26 -0600

    Working on hard mode

--- client.js
+++ client.js
@@ -34,76 +34,58 @@
 //
 // });
 
-
+/*
 //random number generator
 function randomNumber(min, max){
-     return Math.floor(Math.random() * (1 + max - min) + min);
-   }
+return Math.floor(Math.random() * (1 + max - min) + min);
+}
 //sets the winning color to the color with the id that corresponds to the random number
 var winningColor = '';
 var winningColorId = randomNumber(0,4);
 
 if(winningColorId == 0){
-  winningColor = 'red'
+winningColor = 'red'
 }
 if(winningColorId == 1){
-  winningColor = 'blue'
+winningColor = 'blue'
 }
 if(winningColorId == 2){
-  winningColor = 'green'
+winningColor = 'green'
 }
 if(winningColorId == 3){
-  winningColor = 'yellow'
+winningColor = 'yellow'
 }
 if(winningColorId == 4){
-  winningColor = 'purple'
+winningColor = 'purple'
 }
 console.log('the winning color is: ',winningColor);
+*/
+var colorChoices = []; //stores all the color choices and their attributes
+var colorId = 0;
 
 $(document).ready(function(){
+
   console.log('the document has loaded');
 
-  //create the color blocks
-  $('#colorBlockSection').append('<div class="colorBlock" id="0" name="red" data-color="red"></div>');
-  $('#0').css({'width':'100px','height':'150px','display':'inline-block','background-color':'red'});
-  $('#colorBlockSection').append('<div class="colorBlock" id="1" name="blue" data-color="blue" ></div>');
-  $('#1').css({'width':'100px','height':'150px','display':'inline-block','background-color':'blue'});
-  $('#colorBlockSection').append('<div class="colorBlock" id="2" name="green" data-color="green"></div>');
-  $('#2').css({'width':'100px','height':'150px','display':'inline-block','background-color':'green'});
-  $('#colorBlockSection').append('<div class="colorBlock" id="3" name="yellow" data-color="yellow"></div>');
-  $('#3').css({'width':'100px','height':'150px','display':'inline-block','background-color':'yellow'});
-  $('#colorBlockSection').append('<div class="colorBlock" id="4" name="purple" data-color="purple"></div>');
-  $('#4').css({'width':'100px','height':'150px','display':'inline-block','background-color':'purple'});
+  //create the listener for confirming the color choice
+  $('form').on('submit',function(event){
+    event.preventDefault();
+    console.log('confirm button clicked, we picked up this input: ', $(this).serializeArray());
+    var colorArray = $(this).serializeArray(); // [{}, {}, {}]
+    var newColorObject = {}; // {colorName: 'red', id: '0'}
 
-  //create the listener to check for any clicks in each div
-  //and alerts the user the result
-
-  $('.colorBlock').on('click',function(){
-    // console.log('User clicked: ',$(this).data('color'));
-    if($(this).data('color') == winningColor){
-      $('#pickedColor').html(winningColor + ', which happens to be my favorite color. You win!  I have picked a new ' +
-    'color.  Please try again...');
-    var winningColorId = randomNumber(0,4);
-    console.log(winningColorId);
-    if(winningColorId == 0){
-      winningColor = 'red'
-    }
-    if(winningColorId == 1){
-      winningColor = 'blue'
-    }
-    if(winningColorId == 2){
-      winningColor = 'green'
-    }
-    if(winningColorId == 3){
-      winningColor = 'yellow'
-    }
-    if(winningColorId == 4){
-      winningColor = 'purple'
-    }
-    } else {
-      $('#pickedColor').html($(this).data('color') + ', which is not the color I chose.  Please try again');
+    for (var i = 0; i < colorArray.length; i++) {
+      var inputColorObject = colorArray[i];
+      newColorObject[inputColorObject.name] = inputColorObject.value;
+      newColorObject.id = colorId;
+      colorId++;
+      colorChoices.push(newColorObject);
     }
 
-  })
-
+    for (var i = 0; i < colorChoices.length; i++) {
+      var newDiv = $('<div class="colorBlock" id="'+ colorChoices[i].id +'" data-color="'+colorChoices[i].colorName+'">');
+      $('#colorBlockSection').append(newDiv);
+      $('#'+i).css({'width':'200px','height':'200px','display':'inline-block','background-color':''+ colorChoices[i].colorName +''});
+    }
+  });
 });
